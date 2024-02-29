35e9429d322f19598b6955069009ca6d5c54d15d
robby <vladislavgritsuk@gmail.com>
2024-02-29 10:23:14 +0200

    Pr2_3

--- Pr2/CodeFile1.cs
+++ Pr2/CodeFile1.cs
@@ -6,25 +6,25 @@ namespace Pr2.CodeFile1
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("===Task 1===");
-            //Class1 c1 = new Class1();
-            //c1.Task1();
-            //Console.WriteLine();
+            Console.WriteLine("===Task 1===");
+            Class1 c1 = new Class1();
+            c1.Task1();
+            Console.WriteLine();
 
             Console.WriteLine("===Task 2===");
             Class2 c2 = new Class2();
             c2.Task2();
             Console.WriteLine();
 
-            //Console.WriteLine("===Task 3===");
-            //Class3 c3 = new Class3();
-            //c3.Task3();
-            //Console.WriteLine();
+            Console.WriteLine("===Task 3===");
+            Class3 c3 = new Class3();
+            c3.Task3();
+            Console.WriteLine();
 
-            //Console.WriteLine("===Task 4===");
-            //Class4 c4 = new Class4();
-            //c4.Task4();
-            //Console.WriteLine();
+            Console.WriteLine("===Task 4===");
+            Class4 c4 = new Class4();
+            c4.Task4();
+            Console.WriteLine();
         }
     }
 }
